885f348b270c2a5976a31b7d7e446aba021bcb72
paratagas <paratagas@gmail.com>
2020-03-29 18:34:56 +0200

    Code refactor

--- errorHandler.js
+++ errorHandler.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 
 const handleError = function (err, res, statPath) {
   const staticPath = path.resolve(__dirname, statPath);
   
-  console.log('The staticPath is: ' + staticPath);
+  console.log(`The staticPath is: ${staticPath}`);
     
-  fs.readFile("app/404.html", (err, data) => {
-    res.setHeader("Content-Type", "text/html");
+  fs.readFile('app/404.html', (err, data) => {
+    res.setHeader('Content-Type', 'text/html');
     res.writeHead(404);
     res.end(data);
   });
--- websockets-chat-bot.js
+++ websockets-chat-bot.js
@@ -2,15 +2,15 @@ const WebSocket = require('ws');
 const chatClient = new WebSocket('http://localhost:3001');
 const botName = 'Bot';
 
-chatClient.on('open', function open() {
+chatClient.on('open', () => {
   chatClient.send('bot connection opened');
 });
 
 chatClient.on('message', data => {
   console.log(`bot received message: ${data}`);
 
-  // is someone contact "Bot" it answers
-  if (data.indexOf(botName) >= 0) {
+  // is someone contacts "Bot" it answers
+  if (data.includes(botName)) {
     chatClient.send('bot answered');
   }
 });
